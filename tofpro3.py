83ba16758124a7faec67023794dbe73d1b84ed9b
mimi16117 <maria.olguin@estudiantes.colegiotgs.cl>
2025-10-02 15:07:38 -0300

    Update tofpro3.py

--- tofpro3.py
+++ tofpro3.py
@@ -8,5 +8,6 @@ if search_term in ("gatos", "pagina 3", "foto", "pr4", "navegamos"):
     col2.write("Foto gatitos")
     col2.image("OIP(3).jpg", caption="Navegamos hacia el fin del mundo")
     col2.image("OIP(4).jpg")
-    col1.image("ima3.jpg.run")
+    col1.image("ima3.jpg")
+
 
