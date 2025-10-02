dd10be7c517881efb596a8b71617b401d78c58c7
mimi16117 <maria.olguin@estudiantes.colegiotgs.cl>
2025-10-02 15:21:37 -0300

    Update tofpro4.py

--- tofpro4.py
+++ tofpro4.py
@@ -1,5 +1,4 @@
 if search_term in ("pagina 4", "gatos", "pr3", "pr7", "fin del mundo"):
-     search_term = "gatos"
     st.header("🐾 Sección 3 - Página 4", divider=True)
     col1, col2 = st.columns(2)
     col1.video("https://youtu.be/ds-3iZBpaHE?si=6Zlus_rwK9ay7FRx")
@@ -10,6 +9,7 @@ if search_term in ("pagina 4", "gatos", "pr3", "pr7", "fin del mundo"):
     col2.image("pr7.jpg", caption="Navegamos hacia el fin del mundo")
 
     col2.image("ima2.jpg")
+
 
 
 
