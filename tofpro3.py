852c32f5b74e59b28a3e524f138b85bdfd25fce3
mimi16117 <maria.olguin@estudiantes.colegiotgs.cl>
2025-10-02 15:40:58 -0300

    Update tofpro3.py

--- tofpro3.py
+++ tofpro3.py
@@ -1,7 +1,9 @@
 import streamlit as st
+
 search_term = "pagina 3"
+
 if search_term in ("gatos", "pagina 3", "foto", "pr4", "navegamos"):
-     print("Coincidencia encontrada")
+    print("Coincidencia encontrada") # <--- CORREGIDO: 4 espacios
     st.header("🐾 Sección 2 - Página 3", divider=True)
     col1, col2 = st.columns(2)
     col1.video("https://youtu.be/hvi3J3yBRXI?si=BJVJZbSLtGCKCrPZ")
@@ -9,15 +11,6 @@ if search_term in ("gatos", "pagina 3", "foto", "pr4", "navegamos"):
     col1.write("AMAMOS A LOS GATOS")
     col1.write("los gatos más lindos son los gorditos")
     col2.write("Foto gatitos")
-    col2.image("OIP(3).jpg", caption="Navegamos hacia el fin del mundo")
+    col2.image("OIP(3).jpg", caption="Navegamos hacia el fin del mundo") 
     col2.image("OIP(4).jpg")
     col1.image("ima3.jpg")
-
-
-
-
-
-
-
-
-
