d10ba700d15003ae0033e10cc9cab6dc6f0af052
mimi16117 <maria.olguin@estudiantes.colegiotgs.cl>
2025-10-02 15:32:18 -0300

    Update tofpro3.py

--- tofpro3.py
+++ tofpro3.py
@@ -1,3 +1,4 @@
+import streamlit as st
 search_term = "pagina 3"
 if search_term in ("gatos", "pagina 3", "foto", "pr4", "navegamos"):
      print("Coincidencia encontrada")
@@ -11,6 +12,7 @@ if search_term in ("gatos", "pagina 3", "foto", "pr4", "navegamos"):
     col2.image("OIP(3).jpg", caption="Navegamos hacia el fin del mundo")
     col2.image("OIP(4).jpg")
     col1.image("ima3.jpg")
+
 
 
 
