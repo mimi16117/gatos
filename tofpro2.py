50a99f8e83d78d37823c3be3009b46b8178d6fdc
mimi16117 <maria.olguin@estudiantes.colegiotgs.cl>
2025-10-02 15:00:41 -0300

    Update tofpro2.py

--- tofpro2.py
+++ tofpro2.py
@@ -1,4 +1,4 @@
-if search_term in ("", "gatos", "gatitos", "gorditos", "pagina 2", "los gatiros son hermosos"):
+if search_term in ("gatos", "gatitos", "gorditos", "pagina 2", "los gatiros son hermosos"):
     st.header("🐾 Sección 1 - Página 2", divider=True)
     col1, col2 = st.columns(2)
     col1.video("https://youtu.be/hvi3J3yBRXI?si=BJVJZbSLtGCKCrPZ")
@@ -9,4 +9,5 @@ if search_term in ("", "gatos", "gatitos", "gorditos", "pagina 2", "los gatiros 
     col2.write("los perros son muy amables y cariñosos")
     col2.image("OIP(6).jpg", caption="Navegamos hacia el fin del mundo")
     col2.image("OIP(5).jpg")
+
     col1.image("ima6.jpg")
