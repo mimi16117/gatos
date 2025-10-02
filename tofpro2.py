8e8931f619211ec8bdcf7424624c5241db9046fd
mimi16117 <maria.olguin@estudiantes.colegiotgs.cl>
2025-10-02 15:32:35 -0300

    Update tofpro2.py

--- tofpro2.py
+++ tofpro2.py
@@ -1,3 +1,4 @@
+import streamlit as st
 search_term = "gatitos"
 if search_term  in ("gatos", "gatitos", "gorditos", "pagina 2", "los gatiros son hermosos"):
      print("Coincidencia encontrada")
@@ -12,3 +13,4 @@ if search_term  in ("gatos", "gatitos", "gorditos", "pagina 2", "los gatiros son
     col2.image("OIP(6).jpg", caption="Navegamos hacia el fin del mundo")
     col2.image("OIP(5).jpg")
     col1.image("ima6.jpg")
+
